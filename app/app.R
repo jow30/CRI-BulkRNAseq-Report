3ab8a00c14c870511270bd03f1f45395ca037d5b
jow30 <qiaoshan@uchicago.edu>
2024-10-28 10:03:17 -0500

    reload previous parameters

--- app/app.R
+++ app/app.R
@@ -79,6 +79,45 @@ server <- function(input, output, session) {
   # Initialize submit button as disabled
   disable("submit_btn")
   
+  # load parameters from a RData file
+  observe({
+    if (file.exists(file.path(Sys.getenv("PWD"), "params.RData"))) {
+      params <- readRDS(file.path(Sys.getenv("PWD"), "params.RData"))
+      updateTextInput(session, "title", value = params$title)
+      updateTextInput(session, "author", value = params$author)
+      updateTextAreaInput(session, "intro", value = params$intro)
+      updateTextAreaInput(session, "nf_notes", value = params$nf_notes)
+      updateSelectizeInput(session, "multiqc_path", selected = params$multiqc_path)
+      updateTextAreaInput(session, "multiqc_notes", value = params$multiqc_notes)
+      updateSelectizeInput(session, "report_out_dir", selected = params$report_out_dir)
+      updateSelectInput(session, "nextflow_out_dir", selected = params$nextflow_out_dir)
+      updateSelectizeInput(session, "gtf_file", selected = params$gtf_file)
+      updateCheckboxInput(session, "protein_coding", value = params$protein_coding)
+      updateSelectizeInput(session, "metadata_file", selected = params$metadata_file)
+      updateSelectizeInput(session, "sample_remove", selected = params$sample_remove)
+      updateSelectizeInput(session, "color_by", selected = params$color_by)
+      updateSelectizeInput(session, "shape_by", selected = params$shape_by)
+      updateTextInput(session, "top_var", value = params$top_var)
+      updateSelectInput(session, "de_method", selected = params$de_method)
+      updateCheckboxInput(session, "batch_correction", value = params$batch_correction)
+      updateNumericInput(session, "fdr_thres", value = params$fdr_thres)
+      updateNumericInput(session, "fc_thres", value = params$fc_thres)
+      updateNumericInput(session, "ora_fdr_thres", value = params$ora_fdr_thres)
+      updateSelectInput(session, "species", selected = params$species)
+      updateCheckboxInput(session, "ora_all", value = params$ora_all)
+      updateCheckboxInput(session, "ora_go", value = params$ora_go)
+      updateCheckboxInput(session, "ora_kegg", value = params$ora_kegg)
+      updateCheckboxInput(session, "ora_reactome", value = params$ora_reactome)
+      updateCheckboxInput(session, "ora_msigdb", value = params$ora_msigdb)
+      updateSelectInput(session, "ora_msigdbr_category", selected = params$ora_msigdbr_category)
+      updateSelectInput(session, "ora_msigdbr_subcategory", selected = params$ora_msigdbr_subcategory)
+      updateCheckboxInput(session, "gsea_msigdb", value = params$gsea_msigdb)
+      updateNumericInput(session, "gsea_fdr_thres", value = params$gsea_fdr_thres)
+      updateSelectInput(session, "gsea_msigdbr_category", selected = params$gsea_msigdbr_category)
+      updateSelectInput(session, "gsea_msigdbr_subcategory", selected = params$gsea_msigdbr_subcategory)
+    }
+  })
+
   # Observe the selected MSigDB category and update the subcategory options
   observeEvent(input$ora_msigdbr_category, {
     req(input$ora_msigdbr_category)  # Ensure a category is selected
@@ -292,6 +331,9 @@ server <- function(input, output, session) {
       gsea_msigdbr_subcategory = gsea_msigdbr_subcategory_val
     )
     
+    # save parameters to a RData file
+    saveRDS(params, file = file.path(Sys.getenv("PWD"), "params.RData"))
+
     showModal(modalDialog(
       title = "Generating Report",
       div(
